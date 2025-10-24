bb7b4ae91d28a23b64e3830e5b41da7aadde0415
ItisTJ <139549711+ItisTJ@users.noreply.github.com>
2025-10-24 18:08:06 +0530

    check loader deploy

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next"
 import "./global-clean.css"
+import { useEffect, useState } from "react"
+import Loader from "../Components/Loader/Loader"
 
 export const metadata: Metadata = {
   title: 'Thilina Jayasingha - Portfolio',
@@ -11,6 +13,32 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+
+    const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    let maxTimeout
+
+    // 1. Function to hide loader
+    const hideLoader = () => setLoading(false)
+
+    // 2. Wait for all assets to load
+    if (document.readyState === "complete") {
+      hideLoader()
+    } else {
+      window.addEventListener("load", hideLoader)
+    }
+
+    // 3. Ensure loader disappears after 4 seconds no matter what
+    maxTimeout = setTimeout(hideLoader, 4000)
+
+    return () => {
+      window.removeEventListener("load", hideLoader)
+      clearTimeout(maxTimeout)
+    }
+  }, [])
+
+
   return (
     <html lang="en">
       <head>
@@ -18,7 +46,7 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
       <body className="antialiased">
-        {children}
+        {loading ? <Loader /> : <main>{children}</main>}
       </body>
     </html>
   )
