d49c87089436c24f8b25dbf77ad89c9e807677d8
ItisTJ <139549711+ItisTJ@users.noreply.github.com>
2025-10-24 19:50:11 +0530

    loader successull

--- Components/Loader/LoaderWrapper.tsx
+++ Components/Loader/LoaderWrapper.tsx
@@ -8,10 +8,22 @@ export default function LoaderWrapper({ children }: { children: React.ReactNode 
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
-    // Always show loader for 4 seconds
-    const timer = setTimeout(() => setLoading(false), 5000)
+    const hideLoader = () => setLoading(false)
 
-    return () => clearTimeout(timer)
+    // Wait until all assets are loaded
+    if (document.readyState === "complete") {
+      hideLoader()
+    } else {
+      window.addEventListener("load", hideLoader)
+    }
+
+    // Fallback 10s timeout
+    const maxTimeout = setTimeout(hideLoader, 10000)
+
+    return () => {
+      window.removeEventListener("load", hideLoader)
+      clearTimeout(maxTimeout)
+    }
   }, [])
 
   return <>{loading ? <Loader /> : children}</>
